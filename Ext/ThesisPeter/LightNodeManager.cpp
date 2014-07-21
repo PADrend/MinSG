38fde729f20abbe6dc8db16b92968f61b4545092
Peter Stilow <pstilow@mail.upb.de>
2014-07-21 11:48:48 +0200

    added class LightNodeManager to implement global illumination lighting

---
+++ Ext/ThesisPeter/LightNodeManager.cpp
@@ -0,0 +1,25 @@
+/*
+	This file is part of the MinSG library extension ThesisPeter.
+	Copyright (C) 2014 Peter Stilow
+
+	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+	You should have received a copy of the MPL along with this library; see the
+	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+#ifdef MINSG_EXT_THESISPETER
+
+#include "LightNodeManager.h"
+
+#include <Rendering/Mesh/Mesh.h>
+
+namespace MinSG {
+namespace ThesisPeter {
+
+void LightNodeManager::addAttribute(Rendering::Mesh *mesh){
+
+}
+
+}
+}
+
+#endif /* MINSG_EXT_THESISPETER */
