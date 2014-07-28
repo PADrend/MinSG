4808e7b84e2bd16a9d7ca676d1b928e1c719db9e
Peter Stilow <pstilow@mail.upb.de>
2014-07-29 00:28:53 +0200

    added debugging class to c++

---
+++ Ext/ThesisPeter/DebugObjects.cpp
@@ -0,0 +1,74 @@
+/*
+	This file is part of the MinSG library extension ThesisPeter.
+	Copyright (C) 2014 Peter Stilow
+
+	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+	You should have received a copy of the MPL along with this library; see the
+	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+#define MINSG_EXT_THESISPETER
+#ifdef MINSG_EXT_THESISPETER
+
+#include "DebugObjects.h"
+
+#include <Rendering/MeshUtils/MeshBuilder.h>
+#include <Rendering/Mesh/Mesh.h>
+#include <MinSG/SceneManagement/Importer/ImportContext.h>
+
+namespace MinSG {
+namespace ThesisPeter {
+
+DebugObjects::DebugObjects(MinSG::Node* sceneRootNode){
+	this->sceneRootNode = (MinSG::GroupNode*)sceneRootNode;
+	nodeAdded = false;
+}
+
+DebugObjects::~DebugObjects(){
+	if(nodeAdded){
+		sceneRootNode->removeChild(lineNode.get());
+	}
+}
+
+void DebugObjects::addDebugLine(Geometry::Vec3 start, Geometry::Vec3 end, Util::Color4f colStart, Util::Color4f colEnd){
+	DebugLine *line = new DebugLine();
+	line->start = start;
+	line->end = end;
+	line->colorStart = colStart;
+	line->colorEnd = colEnd;
+	linesData.push_back(line);
+}
+
+void DebugObjects::clearDebug(){
+	linesData.clear();
+}
+
+void DebugObjects::buildDebugLineNode(){
+	Rendering::MeshUtils::MeshBuilder mb;
+
+	for(unsigned int i = 0; i < linesData.size(); i++){
+        mb.color(linesData[i]->colorStart);
+        mb.position(linesData[i]->start);
+        mb.addVertex();
+
+        mb.color(linesData[i]->colorEnd);
+        mb.position(linesData[i]->end);
+        mb.addVertex();
+	}
+
+	Rendering::Mesh *mesh = mb.buildMesh();
+	mesh->setUseIndexData(false);
+	mesh->setDrawMode(Rendering::Mesh::DRAW_LINES);
+
+	lineNode.get()->setMesh(mesh);
+
+	if(!nodeAdded){
+		nodeAdded = true;
+		lineNode.get()->setTempNode(true);
+		sceneRootNode->addChild(lineNode.get());
+	}
+}
+
+}
+}
+
+#endif /* MINSG_EXT_THESISPETER */
---
+++ Ext/ThesisPeter/DebugObjects.h
@@ -0,0 +1,51 @@
+/*
+	This file is part of the MinSG library extension ThesisPeter.
+	Copyright (C) 2014 Peter Stilow
+
+	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+	You should have received a copy of the MPL along with this library; see the
+	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+#define MINSG_EXT_THESISPETER
+#ifdef MINSG_EXT_THESISPETER
+
+#ifndef MINSG_THESISPETER_DEBUGOBJECTS_H_
+#define MINSG_THESISPETER_DEBUGOBJECTS_H_
+
+#include <vector>
+#include <Geometry/Vec3.h>
+#include <Util/Graphics/ColorLibrary.h>
+#include <MinSG/Core/Nodes/GeometryNode.h>
+#include <MinSG/Core/Nodes/GroupNode.h>
+
+namespace MinSG {
+namespace ThesisPeter {
+
+struct DebugLine {
+	Geometry::Vec3 start;
+	Geometry::Vec3 end;
+	Util::Color4f colorStart;
+	Util::Color4f colorEnd;
+};
+
+class DebugObjects {
+public:
+	DebugObjects(MinSG::Node* sceneRootNode);
+	~DebugObjects();
+
+	void addDebugLine(Geometry::Vec3 start, Geometry::Vec3 end, Util::Color4f colStart, Util::Color4f colEnd);
+	void clearDebug();
+	void buildDebugLineNode();
+private:
+	Util::Reference<MinSG::GeometryNode> lineNode;
+	std::vector<DebugLine*> linesData;
+	bool nodeAdded;
+	MinSG::GroupNode *sceneRootNode;
+};
+
+}
+}
+
+#endif // MINSG_THESISPETER_DEBUGOBJECTS_H_
+
+#endif // MINSG_EXT_THESISPETER
