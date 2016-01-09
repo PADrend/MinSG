3c8f86efb42d55ffe5dc6e33397cb2b3a6779393
Benjamin Eikel <benjamin@eikel.org>
2016-01-09 11:17:02 +0100

    Shorten descriptions of counters

--- Ext/PipelineStatistics/Statistics.cpp
+++ Ext/PipelineStatistics/Statistics.cpp
@@ -15,17 +15,17 @@ namespace MinSG {
 namespace PipelineStatistics {
 
 Statistics::Statistics(MinSG::Statistics & statistics) {
-	verticesSubmittedCounter = statistics.addCounter("number of transferred vertices", "1");
-	primitivesSubmittedCounter = statistics.addCounter("number of transferred primitives", "1");
-	vertexShaderInvocationsCounter = statistics.addCounter("number of vertex shader invocations", "1");
-	tessControlShaderPatchesCounter = statistics.addCounter("number of patches processed by the tessellation control shader stage", "1");
-	tessEvaluationShaderInvocationsCounter = statistics.addCounter("number of tessellation evaluation shader invocations", "1");
-	geometryShaderInvocationsCounter = statistics.addCounter("number of geometry shader invocations", "1");
-	geometryShaderPrimitivesEmittedCounter = statistics.addCounter("number of primitives emitted by the geometry shader", "1");
-	fragmentShaderInvocationsCounter = statistics.addCounter("number of fragment shader invocations", "1");
-	computeShaderInvocationsCounter = statistics.addCounter("number of compute shader invocations", "1");
-	clippingInputPrimitivesCounter = statistics.addCounter("number of clipping input primitives", "1");
-	clippingOutputPrimitivesCounter = statistics.addCounter("number of clipping output primitives", "1");
+	verticesSubmittedCounter = statistics.addCounter("transferred vertices", "1");
+	primitivesSubmittedCounter = statistics.addCounter("transferred primitives", "1");
+	vertexShaderInvocationsCounter = statistics.addCounter("vertex shader invocations", "1");
+	tessControlShaderPatchesCounter = statistics.addCounter("patches processed by tessellation control shader stage", "1");
+	tessEvaluationShaderInvocationsCounter = statistics.addCounter("tessellation evaluation shader invocations", "1");
+	geometryShaderInvocationsCounter = statistics.addCounter("geometry shader invocations", "1");
+	geometryShaderPrimitivesEmittedCounter = statistics.addCounter("primitives emitted by geometry shader", "1");
+	fragmentShaderInvocationsCounter = statistics.addCounter("fragment shader invocations", "1");
+	computeShaderInvocationsCounter = statistics.addCounter("compute shader invocations", "1");
+	clippingInputPrimitivesCounter = statistics.addCounter("clipping input primitives", "1");
+	clippingOutputPrimitivesCounter = statistics.addCounter("clipping output primitives", "1");
 }
 
 Statistics & Statistics::instance(MinSG::Statistics & statistics) {
