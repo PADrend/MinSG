acd2e96ca887153dc070e317d34d68522a58fdc3
Stan <eppinger@campus.uni-paderborn.de>
2016-11-12 11:36:41 +0100

    Added license.

--- Ext/ThesisStanislaw/PhongGI.cpp
+++ Ext/ThesisStanislaw/PhongGI.cpp
@@ -1,3 +1,12 @@
+/*
+	This file is part of the MinSG library.
+	Copyright (C) 20016 Stanislaw Eppinger
+
+	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+	You should have received a copy of the MPL along with this library; see the
+	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
 #ifdef MINSG_EXT_THESISSTANISLAW
 
 
@@ -25,7 +34,11 @@ State::stateResult_t PhongGI::doEnableState(FrameContext & context, Node * node,
   Rendering::RenderingContext::finish();
   _timer.reset();
 #endif // MINSG_THESISSTANISLAW_GATHER_STATISTICS
-
+  auto camPos = context.getCamera()->getWorldOrigin();
+  auto nodePos = node->getWorldPosition();
+  auto diff = (camPos - nodePos).length();
+  if (diff > 20) return State::stateResult_t::STATE_SKIP_RENDERING;
+  
   auto& rc = context.getRenderingContext();
 
   rc.pushAndSetShader(_shader.get());
--- Ext/ThesisStanislaw/PolygonIndexing.h
+++ Ext/ThesisStanislaw/PolygonIndexing.h
@@ -1,3 +1,11 @@
+/*
+	This file is part of the MinSG library.
+	Copyright (C) 20016 Stanislaw Eppinger
+
+	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+	You should have received a copy of the MPL along with this library; see the
+	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
 
 #ifdef MINSG_EXT_THESISSTANISLAW
 
